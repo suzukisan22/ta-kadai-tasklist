3cf8a67770112aacdbb900d72c9b0291f11f31ec
ryota.suzuki <getty2017@gmail.com>
2018-08-08 16:10:54 +0900

    タスクdone

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: 'タスクが正常に作られました。'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: '正常にタスク内容を更新しました。'
+    else
+      flash.now[:danger] = "正常にタスクの更新ができませんでした"
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: '正常にタスクを削除しました。'
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
